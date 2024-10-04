ce5bce798cc8212588d3267b9091a770c63ef775
Zachary Stence <zachstence@gmail.com>
2024-10-04 13:14:02 -0500

    refactor: move funcs to lib:

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,18 @@
-// place files you want to import through the `$lib` alias in this folder.
+export const PHI = (1 + Math.sqrt(5)) / 2 - 1;
+
+export const polarToCartesian = (r: number, deg: number): { x: number; y: number } => ({
+	x: r * Math.cos(deg),
+	y: r * Math.sin(deg)
+});
+
+export const generatePoints = (
+	deltaR: number,
+	deltaDeg: number,
+	numPoints: number
+): { x: number; y: number }[] => {
+	return Array.from({ length: numPoints }, (_, i) => {
+		const angle = i * deltaDeg * 360;
+		const r = deltaR * Math.floor(angle / 360);
+		return polarToCartesian(r, angle);
+	});
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,17 @@
 <script lang="ts">
-	const PHI = (1 + Math.sqrt(5)) / 2 - 1;
+	import { generatePoints, PHI } from '$lib';
 
-	const deltaTheta = PHI;
-	const svgSize = 500;
-	let deltaR = 0.1;
+	const svgSize = 150;
 	const numPoints = 1000;
 
-	const polarToCartesian = (r: number, deg: number): { x: number; y: number } => ({
-		x: r * Math.cos(deg),
-		y: r * Math.sin(deg)
-	});
+	const deltaDeg = PHI;
+	const deltaR = 0.1;
 
-	const onWheel = (e: WheelEvent) => {
-		if (e.deltaY > 0) {
-			deltaR -= 0.02;
-		} else {
-			deltaR += 0.02;
-		}
-	};
+	const points = generatePoints(deltaR, deltaDeg, numPoints);
 </script>
 
-<svelte:window on:wheel={onWheel} />
-
 <svg class="h-screen w-screen" viewBox="0 0 {svgSize} {svgSize}" xmlns="http://www.w3.org/2000/svg">
-	{#each Array.from({ length: numPoints }) as _, i}
-		{@const angle = i * deltaTheta * 360}
-		{@const r = deltaR * Math.floor(angle / 360)}
-		{@const c = polarToCartesian(r, angle)}
-		<circle cx={c.x + svgSize / 2} cy={c.y + svgSize / 2} r={svgSize / 1000} fill="black" />
+	{#each points as p}
+		<circle cx={p.x + svgSize / 2} cy={p.y + svgSize / 2} r={svgSize / 500} fill="black" />
 	{/each}
 </svg>
